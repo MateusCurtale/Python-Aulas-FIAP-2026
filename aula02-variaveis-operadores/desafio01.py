ef80a5650c342c36f22ada33ebdca0ef31db0e20
MateusCurtale <mateus.curtale@gmail.com>
2026-03-16 22:39:11 -0300

    Desenvolvimento dos desafios de aula

---
+++ aula02-variaveis-operadores/desafio01.py
@@ -0,0 +1,20 @@
+
+# Desafio 01
+nome = input("Digite seu nome por favor: ")
+print(f"Olá {nome}, seja bem vindo(a)!")
+
+# Desafio 02
+
+dia = int(input("Digite o dia em que você nasceu: "))
+mes = int(input("Digite o mês que você nasceu: "))
+ano = int(input("Digite o ano que você nasceu: "))
+
+print(f"você nasceu em: {dia}/{mes}/{ano}")
+
+# Desafio 03 
+
+numero1 = int(input("Digite um número inteiro: "))
+numero2 = int(input("Digite um numero inteiro: "))
+soma = numero1 + numero2
+
+print(f"a soma dos dois números será: {soma}")
