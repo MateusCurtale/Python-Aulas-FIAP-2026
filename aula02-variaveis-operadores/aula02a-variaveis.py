df46ff314b96614539f0bf95037ff0491e0a28a1
MateusCurtale <mateus.curtale@gmail.com>
2026-03-16 22:30:55 -0300

    criação do arquivo desafio e primeiros comandos em python

---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,33 @@
+print("Olá mundo!")
+
+print(7+1)
+print("7 + 1")
+print("7" + " 8") #concatena strings
+
+# Comentarios de 1 linha no python
+
+'''
+    comentários de multiplas
+      linhas
+
+'''
+
+# Váriaveis 
+
+nome = "Alexandre" # string
+print(nome)
+
+idade = 26 # int
+peso = 70.6 # float
+
+print(nome, idade, peso)
+
+# INPUT - SIMULAR UM FORMULÁRIO NO CMD
+nome = input("Digite um nome: ")
+print(nome)
+idade = int(input("Digite sua idade: "))
+peso = float(input("Digite seu peso: "))
+
+print(nome, idade, peso)
+
+print(f"Oiii, {nome}")
