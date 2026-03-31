92f4bcebff2b04cec8b92a14aa9c90ca79d294b6
Mateus Felipe <96144558+MateusCurtale@users.noreply.github.com>
2026-03-30 22:10:38 -0300

    criação do arquivo aula03b

---
+++ aula03-modulos-condicionais/aula03a-modulos.py
@@ -0,0 +1,17 @@
+import math
+import random
+
+num = 17
+raiz = math.sqrt(num)
+
+print(f"a raiz de {num} é: {raiz:.2f}")
+
+graus = 45
+radiano = graus / 180*math.pi
+seno = math.sin(radiano)
+
+num_rand = random.random()
+print(num_rand*10)
+
+num_rand_int = random.randint(1,10)
+print(num_rand_int)
