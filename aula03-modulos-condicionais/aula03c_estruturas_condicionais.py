845d9c07b091a775f265d3d23a72f51edcc7de7a
Mateus Felipe <96144558+MateusCurtale@users.noreply.github.com>
2026-03-30 22:31:01 -0300

    Estruturas condicionais

--- aula03-modulos-condicionais/aula03c_estruturas_condicionais.py
+++ aula03-modulos-condicionais/aula03c_estruturas_condicionais.py
@@ -1,10 +1,35 @@
 # Estruturas condicionais
 
-nota_final = 8
+# Estrutura condicional simples
+nota_final = 4
 
-if nota_final < 6:
+if nota_final < 4:
+    print("Reprovado")
+
+# Estrutura condicional composta
+nota_final = 4
+
+if nota_final < 4:
     print("Reprovado")
 else:
     print("Aprovado")
 
-print("Fim")
+# Estrutura condicional encadeada
+nota_final = 3
+
+if nota_final < 4:
+    print("Reprovado")
+else:
+    if nota_final < 6:
+        print("Recuperação")
+    else:
+        print("Aprovado")
+
+
+# Estrutura condicional encadeada - elif
+if nota_final < 4:
+    print("Reprovado")
+elif nota_final < 6:
+    print("Recuperação")
+else:
+    print("Aprovada")
